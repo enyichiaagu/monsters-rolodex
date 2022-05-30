a280aa4eef30c02c61b9d5d348b4d5b38a16b031
enyichiaagu <enyichiaagu@gmail.com>
2022-05-30 21:22:10 +0100

    Init

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './card.style.css'
+
+export const Card = ({ monster }) => (
+    <div className='card-container'>
+        <img alt='monster' src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} />
+        <h2> {monster.name} </h2 >
+        <p>{monster.email}</p>
+    </div>
+)
